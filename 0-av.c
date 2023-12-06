769b2550bd4ef1e4ddaf108b7df7e8019a47b5b7
gera190293 <gera190293@gmail.com>
2023-12-06 16:58:44 -0300

    pre shell

---
+++ 0-av.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+    (void)argc;
+    int i = 0;
+
+    while (argvv[i])
+        printf("%s\n", argv[i++]);
+    return (0);
+}
