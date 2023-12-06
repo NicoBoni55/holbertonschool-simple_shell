2d3cfab298832a72c66e3826a1e7c3380ac27a8f
gera190293 <gera190293@gmail.com>
2023-12-06 15:06:56 -0300

    pre shell

--- Pre_shell/print_PID.c
+++ Pre_shell/print_PID.c
@@ -1,11 +1,15 @@
-#include "pre_shell.h"
+#include <stdio.h>
+#include <unistd.h>
+
 /**
 * print_PID - print the ID of the parent process
 */
 void print_PID(void)
 {
-    pid_t pid;
+    pid_t pid, ppid;
 
     pid = getpid();
-    printf("%d", pid);
+    ppid = getppid();
+    printf("%d\n", pid);
+    printf("%d\n", ppid);
 } 
