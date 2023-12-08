63d3c5c87defc4ea03d36597e0d49e2e07198f18
gera190293 <gera190293@gmail.com>
2023-12-07 21:35:55 -0300

    pre shell

---
+++ Pre_shell/1-read_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *buffer = NULL;
+	size_t size = 0;
+	int n;
+
+	while (1)
+	{
+		write(1, "$ ", 2);
+		n = getline(&buffer, &size, stdin);
+		if (n == -1)
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (0);
+}
