adbffd713e6c2c3861e397aeae401cc45b927750
gera190293 <gera190293@gmail.com>
2023-12-08 08:48:59 -0300

    pre shell

--- Pre_shell/2-command_line_to_av.c
+++ Pre_shell/2-command_line_to_av.c
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
     char string[] = "el mejor tokenizador del lejano holberton de Uruguay";
-    char del = " ";
+    char del[] = " ";
     char *token;
 
     (void)argc;
