a2f8079f28250fcea0ac13b03fb9b5ff0a489572
gera190293 <gera190293@gmail.com>
2023-12-08 08:42:32 -0300

    pre shell

---
+++ Pre_shell/2-command_line_to_av.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    char string[] = "el mejor tokenizador del lejano holberton";
+    char del = " ";
+    char token;
+
+    (void)argc;
+    (void)argv;
+
+    token = strtok(string, del);
+    if (token)
+    {
+        while (token)
+        {
+            printf("%s\n", token);
+            token = strtok(NULL, del);
+        }
+    }
+}
