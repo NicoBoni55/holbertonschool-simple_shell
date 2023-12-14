035d626ace443267ba2b6dc66a3403a223ed5382
NicoBoni55 <bonifrank55@gmail.com>
2023-12-14 10:30:44 -0800

    Pre shell

--- Pre_shell/shell.c
+++ Pre_shell/shell.c
@@ -3,41 +3,57 @@
 #include<unistd.h>
 #include<string.h>
 #include <sys/wait.h>
+void execc(char *inp)
+{
+	pid_t pid;
+	char *token;
+	char *args[1024];
+	int i = 0, state;
 
-int main()
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		token = strtok(inp, " ");
+		while (token != NULL && i < sizeof(args) / sizeof(args[0]) - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+		execvp(args[0], args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, &state, 0);
+}
+int main(void)
 {
 	char *command = NULL;
-	char *arg[] = {command, NULL};
 	size_t size = 0;
-	int pid, ppid, n;
+	int n;
 
 	while (1)
 	{
-		write(1, "#cisfun$ ",9);
+		write(1, "#cisfun$ ", 9);
 		n = getline(&command, &size, stdin);
 
-			if (n == EOF)
-			{
-				printf("\n");
-				exit(EXIT_FAILURE);
-			}
-			if (strncmp(command, "exit", 4)  == 0)
-			{
-				exit(EXIT_FAILURE);
-			}
-
-			command[strcspn(command, "\n")] = '\0';
-
-			pid = fork();
-			if (pid == 0)
-			{
-				execve(arg[0], arg, NULL);
-				return (0);
-			}
-			else
-			{
-				waitpid(pid, NULL, 0);
-			}
+		if (n == EOF)
+		{
+			printf("\n");
+			exit(EXIT_FAILURE);
+		}
+		if (strncmp(command, "exit", 4)  == 0)
+		{
+			exit(EXIT_FAILURE);
+		}
+		command[strcspn(command, "\n")] = '\0';
+		execc(command);
 	}
-	return(0);
+	return (0);
 }
--- shell.c
+++ shell.c
@@ -1,8 +1,65 @@
 #include "shell.h"
 /**
+ *execc - execute shell commands
+ *@inp : input
+ *
+ *Return: zero
+ */
+void execc(char *inp)
+{
+	pid_t pid;
+	char *token;
+	char *args[1024];
+	int i = 0, state;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		token = strtok(inp, " ");
+		while (token != NULL && i < sizeof(args) / sizeof(args[0]) - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+		execvp(args[0], args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, &state, 0);
+}
+/**
  *main - shell proyect
  *Return: zero
  */
 int main(void)
 {
+	char *command = NULL;
+	size_t size = 0;
+	int n;
 
+	while (1)
+	{
+		write(1, "#cisfun$ ", 9);
+		n = getline(&command, &size, stdin);
+
+		if (n == EOF)
+		{
+			printf("\n");
+			exit(EXIT_FAILURE);
+		}
+		if (strncmp(command, "exit", 4)  == 0)
+		{
+			exit(EXIT_FAILURE);
+		}
+		command[strcspn(command, "\n")] = '\0';
+		execc(command);
+	}
+	return (0);
+}
