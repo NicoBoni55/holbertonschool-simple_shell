5ef37991db68855bc9c644a5045d9a68d672d193
NicoBoni55 <bonifrank55@gmail.com>
2024-04-24 12:59:30 +0000

    shell token

--- which.c
+++ which.c
@@ -2,15 +2,18 @@
 int main(void)
 {
 	char *path = get_env("PATH");
+	int i;
+	char *direct;
 	char *token;
 
-	token = strtok(path, ":");
+	direct = strdup(path);
+	token = strtok(direct, ":");
 	printf("%s\n", token);
-	while (token)
+	for (i = 0; path[i]; i++)
 	{
-		token = strtok(NULL, ":");
+		token = strtok(NULL, ":");	
 		printf("%s\n", token);
 	}
-
+	free(direct);
 	return (0);
 }
